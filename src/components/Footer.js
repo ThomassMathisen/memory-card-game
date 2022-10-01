24e744eec20a2b9bd574e900e6fd714d37ce61c1
ThomassMathisen <thomasmathisen@hotmail.com>
2022-10-01 11:58:16 +0200

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="header">
+      <div>
+        <p>Built by Thomas Storbakk Mathisen</p>
+      </div>
+    </footer>
+  );
+}
