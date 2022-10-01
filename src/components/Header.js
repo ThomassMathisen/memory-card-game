6bbb05172f932220dc215af2691a2fb353b03828
ThomassMathisen <thomasmathisen@hotmail.com>
2022-10-01 11:56:19 +0200

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Scoreboard from "./Scoreboard";
+
+export default function Header(props) {
+  return (
+    <header className="header">
+      <div>
+        <h1>Memory Card Game</h1>
+        <p>Don't click on the same card twice!</p>
+      </div>
+      <Scoreboard score={props.scores} />
+    </header>
+  );
+}
