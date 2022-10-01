0ca2943581a89f24df71842d044281fea5f5f6c0
ThomassMathisen <thomasmathisen@hotmail.com>
2022-10-01 12:06:53 +0200

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import React, { useState } from "react"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
+
+  const [scores, setScores] = useState({best: 0, current: 0})
   return (
+    <div className="App">
+      <Header scores={scores} />
+      <Main setScores={setScores} />
+      <Footer />
+    </div>
   );
 }
 
